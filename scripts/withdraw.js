fdb4eee64049ecd3c378f3a57dd85d39d9786a62
hidekiriyuga <arshmanva21@gmail.com>
2022-07-05 01:34:24 +0530

    initial commit

---
+++ scripts/withdraw.js
@@ -0,0 +1,15 @@
+const { getNamedAccounts, ethers } = require("hardhat")
+async function main() {
+    const { deployer } = await getNamedAccounts()
+    const fundMe = await ethers.getContract("FundMe", deployer)
+    console.log("Funding contract. . . . . .")
+    const transactionResponse = await fundMe.withdraw()
+    await transactionResponse.wait(1)
+    console.log("withdrawl successfull!!")
+}
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
